4a9c59391e88f38688857311f06469df223da6cc
fernandes99 <fernandes.r1999@gmail.com>
2024-02-13 09:32:37 -0300

    feat: add socket url in env client side

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,7 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = 'https://triunfoverso.onrender.com:8000/';
+const URL = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:8000';
 
 export const socket = io(URL);
-
-// export const socket = new WebSocket('ws://localhost:8000/ws')
